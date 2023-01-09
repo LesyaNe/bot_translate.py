66bf4defd8b68bff70b65b0fc2efaa66916c02c8
LesyaNe <lese4ka81@yandex.ru>
2023-01-09 22:36:55 +0500

    добавила сообщение перевожу

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import telebot
 from googletrans import Translator
+import os
 
-bot = telebot.TeleBot('')  # токен
+bot = telebot.TeleBot('5927677920:AAHMktLf6_RRoc9piTA3mP5pzkRmBjGTmq4')  # токен
 
 
 @bot.message_handler(commands=['start'])
-def send_start(message):
-    bot.reply_to(message, 'Привет, я бот-переводчик английского языка. Напиши слово, и я переведу')  # сообщение \start
+def to_start(message):  # обращаемся к переводчику
+    bot.send_message(message.chat.id,
+                     "Привет, я бот-переводчик английского языка. Напиши слово, и я переведу")  # сообщение \start
 
 
-@bot.message_handler(content_types='text')  # текст пользователя
+# @bot.message_handler(content_types='text')  # текст пользователя
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_text1(message):
+    bot.send_message(message.chat.id, 'Перевожу')
+
 def do_translate(message):
-    translator = Translator()  # обращяемся к переводчику
-    lang = translator.detect(message.text)  # определяем язык
+    translator = Translator()
+    txt = translator.detect(message.text)  # определяем язык
+    if txt[0] == 'ru':  # если язык русский
+        transed = translator.translate(message.text,
+                                       txt_tgt='en')  # переводим с русского на анг.
+        bot.send_message(message.chat.id, transed)  # отправляем перевод
 
-    if lang[0] == 'ru':  # если язык русский
-        transed = translator.translate(message.text, lang_tgt='en')  # переводим с русского на анг.
-        bot.reply_to(message, transed)  # отправляем перевод
-
-    elif lang[0] == 'en':  # если язык английский
-        transed = translator.translate(message.text, lang_tgt='ru')  # переводит с английского на русский
-        bot.reply_to(message, transed)
+    elif txt[0] == 'en':  # если язык английский
+        transed = translator.translate(message.text,
+                                       txt_tgt='ru')  # переводит с английского на русский
+        bot.send_message(message.chat.id, transed)  # отправляем перевод
 
     else:
-        bot.reply_to(message, 'Я вас не понял')  # если пользователь ввел некорректный текс
+        bot.send_message(message.chat.id, 'Я вас не понял')  # если пользователь ввел некорректный текс
 
 
 if __name__ == '__main__':
